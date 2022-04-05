c679cf506e84109bd8ec339b803075596f1a7cdf
Phuoc2406 <95359035+Phuoc2406@users.noreply.github.com>
2022-04-05 11:49:07 +0700

    sua lan 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="<?php echo asset('assets/css/style.css')?>" type="text/css">
 </head>
 <body>
 
